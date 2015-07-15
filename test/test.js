371ee25c871e34765513e7bb839a73a3bb9f120b
John Otander <johnotander@gmail.com>
2015-07-15 13:41:50 -0600

    Add tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,16 @@
-var assert = require('assert');
-var whoisCli = require('..');
+require('shelljs/global')
+var assert = require('assert')
+var whoisCli = require('..')
 
 describe('whois-cli', function() {
 
-  it('should do something awesome', function() {
-    assert.equal(whoisCli(), true);
-  });
-});
+  it('should retrieve whois data', function() {
+    var output = exec('./index.js google.com').output
+    assert.ok(output)
+  })
+
+  it('should provide error text if given an invalid url', function() {
+    var output = exec('./index.js foo').output
+    assert.equal(output, 'Please provide a valid url\n')
+  })
+})
