d8e0739f98bdfbb2022adadb5f21159e6b4eb16c
John Otander <johnotander@gmail.com>
2015-07-15 13:33:16 -0600

    Validate url

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 var meow = require('meow')
 var isUrl = require('is-url')
 var whois = require('node-whois')
+var normalizeUrl = require('normalize-url')
 
 var cli = meow({
   help: [
@@ -13,11 +14,17 @@ var cli = meow({
   ]
 })
 
-whois.lookup(cli.input[0], function(err, data) {
-  if (err) {
-    console.log('whois-cli encountered an error')
-    console.log(err)
-  } else {
-    console.log(data)
-  }
-})
+var url = cli.input[0] && normalizeUrl(cli.input[0])
+
+if (isUrl(url)) {
+  whois.lookup(cli.input[0], function(err, data) {
+    if (err) {
+      console.log('whois-cli encountered an error')
+      console.log(err)
+    } else {
+      console.log(data)
+    }
+  })
+} else {
+  console.log('Please provide a valid url')
+}
